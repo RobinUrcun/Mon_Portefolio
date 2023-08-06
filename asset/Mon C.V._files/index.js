fc2abf6dd4181d3eba0a42c24fe63baf141f236b
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2023-08-06 16:53:42 +0200

    Premier comit

---
+++ asset/Mon C.V._files/index.js
@@ -0,0 +1,53 @@
+const button_up = document.querySelector(".button_up")
+const nav_bar = document.querySelector(".top_navbar")
+const footer_telephone = document.querySelector(".footer_telephone")
+const footer_li_telephone = document.querySelector(".footer_li_telephone")
+const footer_enveloppe = document.querySelector(".footer_enveloppe")
+const footer_li_enveloppe = document.querySelector(".footer_li_enveloppe")
+let btnRadio = document.querySelectorAll("input[type=radio]")
+const sectionEtudes = document.getElementById("section_etudes")
+const sectionExperiences = document.getElementById("section_experiences")
+const label_etudes = document.getElementById("label_etudes")
+const label_experiences = document.getElementById("label_experiences")
+const li_navbar = document.getElementsByClassName("test")
+console.log(li_navbar);
+
+li_navbar.addEventListener("mouseenter", (e) => {
+    li_navbar.style.transform = "scale(2)"
+})
+
+button_up.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+window.addEventListener("scroll", (e) =>{
+    if ( window.scrollY > 400){
+        button_up.style.bottom = "30px";  
+        nav_bar.style.top = "-50px"
+    }
+    else{
+        button_up.style.bottom = "-80px";  
+        nav_bar.style.top = "0px"
+    }
+})
+
+for (let index = 0; index < btnRadio.length; index++){
+    btnRadio[index].addEventListener("change", (e) =>{
+        if(e.target.value == 2){
+            sectionEtudes.style.display = "none"
+            sectionExperiences.style.display = "block"
+            label_experiences.style.border = "4px inset #dddddd"
+            label_etudes.style.border = "4px outset #dddddd"
+
+        }else{
+            sectionEtudes.style.display = "block"
+            sectionExperiences.style.display = "none"
+            label_etudes.style.border = "4px inset #dddddd"
+            label_experiences.style.border = "4px outset #dddddd"
+
+
+        }
+    })
+}
